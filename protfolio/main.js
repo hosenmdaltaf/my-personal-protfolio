d3d0af795b743e482c92429c69cbd6620d9f9ce2
Hosen MD Altaf <64326036+Altaf-H-Miazee@users.noreply.github.com>
2020-09-07 18:48:25 +0800

    And all files from local machine to github
    Its first step will change lot of design

---
+++ protfolio/main.js
@@ -0,0 +1,10 @@
+const Readmorebtn = document.querySelector('.Read-more-btn');
+const text = document.querySelector('.text');
+
+Readmorebtn.addEventListener('click',(e)=>{
+    text.classList.toggle('show-more');
+}
+
+)
+
+
